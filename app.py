4793c94899f6f438710744cd6a523f06f455c5e9
BasingGit <github@hollandfamily.me.uk>
2024-07-09 22:11:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='Steve')
 
-''' This works, but the one below is more common 
 @app.route('/<name>')
 def named(name):
     return render_template('index.html', name=name)
-'''
 
+''' This is the more common notation, but doesn't work in this instance... 
 @app.route('<path:name>')
 def named(name):
 	render_template('index.html', name=name)
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
