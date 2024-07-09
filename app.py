8797975f3c95913e645743e262b0525308274ad8
BasingGit <github@hollandfamily.me.uk>
2024-07-09 22:09:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
-    return render_template('index.html', name=name)
+@app.route('/')
+def index():
+    return render_template('index.html', name='Steve')
 
+''' This works, but the one below is more common 
+@app.route('/<name>')
+def named(name):
+    return render_template('index.html', name=name)
+'''
+
+@app.route('<path:name>')
+def named(name):
+	render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
